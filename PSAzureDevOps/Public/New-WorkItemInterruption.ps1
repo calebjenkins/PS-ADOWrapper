86b600e61ba66a67609066186aa3cf6b52ef3f0b
Caleb Jenkins <calebjenkins@users.noreply.github.com>
2025-11-09 15:12:48 -0600

    Seperating generic new item from new interuption item. More flexible and a good pattern for future extensions

---
+++ PSAzureDevOps/Public/New-WorkItemInterruption.ps1
@@ -0,0 +1,31 @@
+
+function New-WorkItemInterruption {
+    param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [string]$Title,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Type = "Task",
+
+        [Parameter(Mandatory = $false)]
+        [string]$Description = ""
+    )
+
+    # Add the "Interruption" tag to the existing tags
+    if ($PSBoundParameters.ContainsKey('Tags')) {
+        $PSBoundParameters['Tags'] = if ([string]::IsNullOrWhiteSpace($PSBoundParameters['Tags'])) {
+            "Interruption"
+        }
+        else {
+            $PSBoundParameters['Tags'] + ";Interruption"
+        }
+    }
+    else {
+        $PSBoundParameters['Tags'] = "Interruption"
+    }
+
+    New-WorkItem @PSBoundParameters
+}
+
+# Create alias for quick access
+Set-Alias -Name wi-i -Value New-WorkItemInterruption
